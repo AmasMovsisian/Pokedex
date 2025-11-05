87586b835fe416caf8f006707a8c34175297cece
AmasMovsisian <232450988+AmasMovsisian@users.noreply.github.com>
2025-11-05 08:16:34 +0100

    update

--- script.js
+++ script.js
@@ -3,18 +3,39 @@ function init() {
 }
 
 function renderAllPokemon() {
-    fetch('https://pokeapi.co/api/v2/pokemon')
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=24')
         .then(res => res.json())
         .then(data => {
-            console.log(data);
             let results = data.results;
-
             let pokemonContainerRef = document.getElementById("pokemonContainer");
             pokemonContainerRef.innerHTML = '';
 
             for (let i = 0; i < results.length; i++) {
-                pokemonContainerRef.innerHTML += `<p>${results[i].name}</p>`;
+                let pokemon = results[i];
+                let url = pokemon.url;
+
+                fetch(url)
+                    .then(res => res.json())
+                    .then(details => {
+                        let name = details.name;
+                        let image = details.sprites.front_default;
+                        let typeName = details.types[0].type.name;
+
+
+                        if (details.types.length > 1) {
+                            typeName += ' / ' + details.types[1].type.name;
+                        }
+
+                        pokemonContainerRef.innerHTML += `
+                            <div class="pokemon">
+                                <h3>${name}</h3>
+                                <img src="${image}" alt="${name}">
+                                <p>Typ: ${typeName}</p>
+                            </div>
+                        `;
+                    })
+                    .catch(err => console.error('Fehler beim Laden der Pokémon-Details:', err));
             }
         })
-        .catch(err => console.error('Error fetching Pokémon:', err));
+        .catch(err => console.error('Fehler beim Laden der Pokémon-Liste:', err));
 }
